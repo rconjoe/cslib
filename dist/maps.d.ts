818f20eebe2b29e33b49b1e773a64a531d62d653
Ian Lucas <ianlucas_ferreira@hotmail.com>
2023-06-19 23:34:58 -0300

    sync

--- dist/maps.d.ts
+++ dist/maps.d.ts
@@ -14,4 +14,5 @@ export declare const CS_VERTIGO_MAP: CS_Map;
 export declare const CS_OLD_CBBLE_MAP: CS_Map;
 export declare const CS_OLD_CACHE_MAP: CS_Map;
 export declare const CS_ACTIVE_MAP_POOL: CS_Map[];
+export declare const CS_ALL_MAPS: CS_Map[];
 export declare function CS_getMapCommand(map: CS_Map): string;
